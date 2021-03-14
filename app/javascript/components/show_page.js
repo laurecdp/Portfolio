495868b3a78da71cb84dfa6782f02a8db8a95f70
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-14 18:17:18 +0100

    add changes on show

--- app/javascript/components/show_page.js
+++ app/javascript/components/show_page.js
@@ -1,19 +1,24 @@
 const showPage = () => { 
+
+  window.addEventListener("load", (event) => {
+    
+  }) 
+  
   const showCards = document.querySelector(".container-cards");
   const showProjects = document.querySelector(".container-projects");
-
   const buttonProfile = document.getElementById("icon-profile");
+  const buttonProjets = document.getElementById("icon-projets");
 
   buttonProfile.addEventListener("click", (event) => {
     event.preventDefault();
     showCards.classList.toggle("hidden");
+    showProjects.classList.add("d-none");
   })
-
-  const buttonProjets = document.getElementById("icon-projets");
 
   buttonProjets.addEventListener("click", (event) => {
     event.preventDefault();
-    showProjects.classList.toggle("hidden");
+    showProjects.classList.toggle("d-none");
+    showCards.classList.add("hidden");
   })
 
 };
