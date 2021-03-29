ca3c3a27f86228021292d964afaba538c043e82c
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-29 18:11:11 +0200

    add taskbar on the bottom

--- app/javascript/components/loading_page.js
+++ app/javascript/components/loading_page.js
@@ -1,50 +1,35 @@
 const loadingPage = () => {
 
-  function submitPassword() {
-    var loginPage = document.getElementById("login-page");
-    var submit = document.getElementById("submit");
+  const loginPage = document.getElementById("login-page");
+  const userIcon = document.querySelector('.user-icon-bg');
+  const gif = document.getElementById('windows-gif');
+
+  userIcon.addEventListener("click", (event) => {
+    event.preventDefault();
+    setTimeout(function () {
+    loginPage.style.display = " none";
+    }, 100);
 
     setTimeout(function () {
-      submit.style.backgroundColor = "green";
-    }, 1100);
+      gif.style.display = "d-none";
+    }, 1500);
 
-    setTimeout(function () {
-      loginPage.style.display = "none";
-    }, 2000);
-  }
+    document.querySelector(".main").classList.remove("d-none");
+    document.querySelector(".container-icons").classList.remove("d-none");
+    document.querySelector(".taskbar-horizontal").classList.remove("d-none");
 
-  document.addEventListener("keydown", formValidation);
+  })
 
-  function formValidation(e) {
-    if (13 == e.keyCode) {
-      var loginPage = document.getElementById("login-page");
-      var homePage = document.getElementById("home-page")
-      var submitBtn = document.getElementById("submit");
-      var submitForm = document.getElementById("input-container");
-
-      setTimeout(function () {
-        submitBtn.style.backgroundColor = "green";
-      }, 1100);
-
-      setTimeout(function () {
-        loginPage.style.display = "none";
-      }, 2000);
-    }
-  }
-
-  //Toggle Menu
-  function toggleMenu() {
-    var startUpMenu = document.getElementById("startup-menu");
+    //Toggle Menu
+    const startUpMenu = document.getElementById("startup-menu");
     document.getElementById("startup-button").classList.toggle("active");
 
-    if (startUpMenu.style.left === "-400px") {
-      return (startUpMenu.style.left = "0");
-    } else {
-      startUpMenu.style.left = "-400px";
-    }
-  }
-
-
+      if (startUpMenu.style.left === "-400px") {
+        return (startUpMenu.style.left = "0");
+      } else {
+        startUpMenu.style.left = "-400px";
+      }
+  
 };
 
 export { loadingPage };
