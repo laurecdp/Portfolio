95e79767c395dfdeebfaf7f1f3fe4e4b0fb683d3
Laure Curallucci de Peretti <laurecdp@gmail.com>
2021-03-30 13:51:15 +0200

    taskbar ongoing

--- app/javascript/components/toggle_menu.js
+++ app/javascript/components/toggle_menu.js
@@ -1,13 +1,10 @@
 const toggleMenu = () => {
-
   const dropupToggle = document.querySelector(".dropdown-toggle")
-  const windowsIcon = document.querySelector(".fa-windows")
   const dropupMenu = document.querySelector(".dropdown-menu")
   dropupToggle.addEventListener("click", (event) => {
     event.preventDefault();
-    dropupMenu.classList.toggle("d-none");
-  }) 
-
+    dropupMenu.classList.toggle("d-block");
+  })
 };
 
 export { toggleMenu };
